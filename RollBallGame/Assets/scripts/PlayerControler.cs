3be88a158de9adcb293195dda87a32b554aeadd0
Bill <bill@floatyun.com>
2016-07-30 23:22:15 +0800

    init proj.

---
+++ RollBallGame/Assets/scripts/PlayerControler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+	public float Speed;
+
+	Rigidbody mRb;
+
+	void Start()
+	{
+		mRb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float x = Input.GetAxis ("Horizontal");
+		float z = Input.GetAxis ("Vertical");
+		Vector3 mov = new Vector3 (x, 0.0f, z);
+		mRb.AddForce (mov * Speed);
+	}
+
+}
